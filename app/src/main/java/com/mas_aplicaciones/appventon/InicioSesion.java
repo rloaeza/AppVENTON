565ee2e0583b7c201c32c87351a29dda64168aef
vicente <vicente_prez@hotmail.com>
2019-05-22 14:49:04 -0500

    cambiar las variables en inicio sesion y crear primeros  conexiones a los fragment

--- app/src/main/java/com/mas_aplicaciones/appventon/InicioSesion.java
+++ app/src/main/java/com/mas_aplicaciones/appventon/InicioSesion.java
@@ -2,10 +2,17 @@ package com.mas_aplicaciones.appventon;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.navigation.Navigation;
+
+import static androidx.navigation.Navigation.findNavController;
 
 
 /**
@@ -23,7 +30,24 @@ public class InicioSesion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio_sesion, container, false);
+        View view = inflater.inflate(R.layout.fragment_inicio_sesion, container, false);//
+       // Button btnRegistrar = view.findViewById(R.id.botonRegistrar);
+        /*btnRegistrar.setOnClickListener(new View.OnClickListener() {//acomoda y luego muestra y realiza todo lo necesario
+            @Override
+            public void onClick(View v) {
+
+
+                findNavController(v).navigate(R.id.action_inicioSesion_to_tipoUsuario);
+            }
+        });*/
+       // btnRegistrar.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_inicioSesion_to_tipoUsuario));// metodo para pasar al siguiente fragment  sin validar
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {//pesado spinner hace todo despues de que carga el layout facebook
+        super.onViewCreated(view, savedInstanceState);
+        Button btnRegistrar = view.findViewById(R.id.botonRegistrar);
+        btnRegistrar.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_inicioSesion_to_tipoUsuario));// metodo para pasar al siguiente fragment  sin validar
+    }
 }
